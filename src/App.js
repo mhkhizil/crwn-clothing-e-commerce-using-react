267737bbd2a5e575593a682fcaff209c9fd501d7
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-12-17 14:21:34 +0630

    adding linkd

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import React from 'react'
 
 
 
+
+import {  Route, Routes } from 'react-router-dom'
 import Home from './routes/home/home.component'
-import { Route, Routes } from 'react-router-dom'
+import Navigation from './routes/Nav/navigation.component'
+
+
 const App = () => {
 
 
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation/>}>
+        <Route  index element={<Home/>}/>
+     </Route>
     </Routes>
   )
 }
