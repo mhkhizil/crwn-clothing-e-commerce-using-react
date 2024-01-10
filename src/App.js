297ddb669934b862d509f050c8d58705ded61b1b
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-01-10 21:25:34 +0630

    checkout page initiation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Navigation from './routes/Nav/navigation.component'
 
 import Authentication from './routes/authentication/Authentication.component'
 import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
         <Route  index element={<Home/>}/>
         <Route  path='auth' element={<Authentication/>}/>
         <Route  path='shop' element={<Shop/>}/>
+        <Route  path='checkout' element={<Checkout/>}/>
      </Route>
     </Routes>
   )
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./checkout.style.scss"
+const Checkout = () => {
+  return (
+    <div>
+      <h1>I am the checkout page</h1>
+    </div>
+  )
+}
+
+export default Checkout
