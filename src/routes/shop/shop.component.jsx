37db8293e8c7c7342ab07dd6da18c8793b9f112f
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-01-20 16:48:46 +0630

    adding preview

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,25 +3,18 @@ import React, { useContext } from 'react'
 import ProductCard from '../../components/product-card/product-card.component';
 import "./shop.style.scss"
 import { CategoriesContext } from '../../components/contexts/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 const Shop = () => {
     const {categoriesMap}=useContext(CategoriesContext);
     console.log(categoriesMap);
   return (
-<>
+<div className='shop-container'>
 
         {
         Object.keys(categoriesMap).map(title=>{
+          const products=categoriesMap[title];
        return(
-        <>
-        <h1>{title}</h1>
-        <div className='products-container'>
-        {
-          categoriesMap[title]?.map((product)=>(
-          <ProductCard key={product.id} product={product}/>
-          ))
-      } 
-        </div>
-        </>
+       <CategoryPreview key={title} products={products} title={title}/>
        )
         
         })
@@ -31,7 +24,7 @@ const Shop = () => {
         }
   
 
-</>
+</div>
    
   )
 }
