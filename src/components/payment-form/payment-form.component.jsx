88593d30148c295aefb7744abe40f6089b7eb41d
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-02-13 19:00:18 +0630

    setting up stripe

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,13 +1,22 @@
-import {CardElement} from "@stripe/react-stripe-js"
+import {CardElement,useElements,useStripe} from "@stripe/react-stripe-js"
 import CustomButton from "../button/customButom.component"
+import { FormContainer, PaymentFormContainer } from "./payment-form.styles";
 const PaymentForm=()=>{
+    const elements=useElements();
+    const stripe=useStripe();
+    const preventHandler=async(e)=>{
+        e.preventDefault();
+    }
 return(
-    <div>
+    <PaymentFormContainer>
+        <FormContainer>
+            <h2>Credit card payment</h2>
         <CardElement/>
         <CustomButton btnType={"inverted"} >
             Pay now
         </CustomButton>
-    </div>
+        </FormContainer>
+    </PaymentFormContainer>
 )
 }
 export default PaymentForm;
--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -3,5 +3,12 @@
  height:300px;
  display:flex;
  flex-direction:column;
+ align-items:center;
+ justify-content:center;
+
  
+ `
+ export const FormContainer=styled.div`
+ height:100px;
+ min-width:500px;
  `
